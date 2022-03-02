97cb26a11c08e5f703b38b90f1a8fe6c416b6ee0
alihaiderdev <ah1675179@gmail.com>
2022-03-02 12:17:45 +0500

    make structure for client app

--- client/src/App.js
+++ client/src/App.js
@@ -69,76 +69,77 @@ const App = () => {
       console.log(`Error: ${e.message}`);
     }
   };
+
   return (
     <Container className='appScreen py-5'>
       <h1 className='d-flex justify-content-center'>Signup Form</h1>
-      {/* {isLoading ? (
+      {isLoading ? (
         <div className='loaderWrapper'>
           <Spinner animation='border' variant='primary' size='lg' />
         </div>
-      ) : ( */}
-      <Form onSubmit={signup}>
-        <Form.Group className='mb-3' controlId='firstName'>
-          <Form.Label>First Name</Form.Label>
-          <Form.Control
-            type='text'
-            name='firstName'
-            value={firstName}
-            onChange={onChangeHandler}
-            placeholder='Enter your first name'
-          />
-        </Form.Group>
+      ) : (
+        <Form onSubmit={signup}>
+          <Form.Group className='mb-3' controlId='firstName'>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type='text'
+              name='firstName'
+              value={firstName}
+              onChange={onChangeHandler}
+              placeholder='Enter your first name'
+            />
+          </Form.Group>
 
-        <Form.Group className='mb-3' controlId='lastName'>
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            type='text'
-            name='lastName'
-            value={lastName}
-            onChange={onChangeHandler}
-            placeholder='Enter your last name'
-          />
-        </Form.Group>
+          <Form.Group className='mb-3' controlId='lastName'>
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type='text'
+              name='lastName'
+              value={lastName}
+              onChange={onChangeHandler}
+              placeholder='Enter your last name'
+            />
+          </Form.Group>
 
-        <Form.Group className='mb-3' controlId='email'>
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type='email'
-            name='email'
-            value={email}
-            onChange={onChangeHandler}
-            placeholder='Enter your email'
-          />
-        </Form.Group>
+          <Form.Group className='mb-3' controlId='email'>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type='email'
+              name='email'
+              value={email}
+              onChange={onChangeHandler}
+              placeholder='Enter your email'
+            />
+          </Form.Group>
 
-        <Form.Group className='mb-3' controlId='password'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            name='password'
-            value={password}
-            onChange={onChangeHandler}
-            placeholder='Enter your password'
-          />
-        </Form.Group>
-        <Button variant='primary' type='submit'>
-          {isLoading && (
-            <>
-              <Spinner
-                as='span'
-                animation='border'
-                size='sm'
-                role='status'
-                aria-hidden='true'
-                className='me-3'
-              />
-              <span className='visually-hidden'>Loading...</span>
-            </>
-          )}
-          Signup
-        </Button>
-      </Form>
-      {/* )} */}
+          <Form.Group className='mb-3' controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              name='password'
+              value={password}
+              onChange={onChangeHandler}
+              placeholder='Enter your password'
+            />
+          </Form.Group>
+          <Button variant='primary' type='submit'>
+            {isLoading && (
+              <>
+                <Spinner
+                  as='span'
+                  animation='border'
+                  size='sm'
+                  role='status'
+                  aria-hidden='true'
+                  className='me-3'
+                />
+                <span className='visually-hidden'>Loading...</span>
+              </>
+            )}
+            Signup
+          </Button>
+        </Form>
+      )}
     </Container>
   );
 };
